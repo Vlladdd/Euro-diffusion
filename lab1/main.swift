14ab3d1438b7e2d5d6e7ca14d23ef7a22abd6777
Vlad Nechyporenko <vlladdd28@gmail.com>
2020-12-03 00:41:32 +0200

    Initial Commit

---
+++ lab1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  lab1
+//
+//  Created by Vlad Nechiporenko on 12/3/20.
+//  Copyright © 2020 Vlad Nechyporenko. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
